c23c9b31af232940c4bf1385f54cfe47e24aee88
Mohanad Nassar <mmn4work2@gmail.com>
2025-07-30 21:31:55 +0000

    Double the margin one more time

--- src/app/(app)/notes/page.tsx
+++ src/app/(app)/notes/page.tsx
@@ -271,7 +271,7 @@ export default function NotesPage() {
   }
 
   return (
-    <div className="space-y-8 px-8">
+    <div className="space-y-8 px-16">
        <Breadcrumb>
         <BreadcrumbList>
             <BreadcrumbItem>
